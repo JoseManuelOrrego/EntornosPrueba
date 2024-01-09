28a19974eed2b06036f0d9bee9991c847d2521be
pc <pc@172.20.10.2>
2024-01-09 21:52:02 +0100

    CILINDRO

--- EntornosGitHubb/src/areas/Areas.java
+++ EntornosGitHubb/src/areas/Areas.java
@@ -33,6 +33,11 @@ public class Areas
 		return 2 * 3.1415 * radio;
 	}
 	
+	public static double calcularAreaCilindro(double radio, double altura) 
+    {
+        return 2 * 3.1415 * radio * (radio + altura);
+    }
+	
 	public static double calculoAreaRectangulo(double base, double altura)
 	{
 		return base*altura;
